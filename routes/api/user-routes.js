d1673deb557fffbbcbc967a20f375ec826a15169
Drew Zinck <dlzincktheii@gmail.com>
2022-05-13 18:41:53 -0700

    fixed syntax and typos

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,22 +1,22 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  addToFriendList,
-  removefromFriendList,
-} = require("../../controllers/userController");
+	getAllUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+	addToFriendList,
+	removefromFriendList,
+} = require('../../controllers/user-controllers');
 
 // Set up GET all and POST at /api/users. Provide name of controller as callback
-router.route("/").get(getAllUsers).post(createUser);
+router.route('/').get(getAllUsers).post(createUser);
 
 // Set up GET one, PUT, and DELETE at /api/users/<id>
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
-router.route("/:userId/friends/:friendId").post(addToFriendList);
+router.route('/:userId/friends/:friendId').post(addToFriendList);
 
-router.route("/:userId/friends/:friendId").delete(removefromFriendList);
+router.route('/:userId/friends/:friendId').delete(removefromFriendList);
 
 module.exports = router;
